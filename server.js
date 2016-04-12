2ccee7b6f1e8f0bfc808d7942031498c3f9d51ed
unknown <akashk515@5CG4490BJS.IN.nam.ad.pwcinternal.com>
2016-04-12 14:38:04 +0530

    final test

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.static(__dirname + '/public'));
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-app.listen(port, ip, function(){
+app.listen(port, function(){
 	console.log('Running the server at:',ip,':',port);
 });
 
